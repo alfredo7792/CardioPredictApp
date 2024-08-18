e0c0380fab3ff25651f8c06ac41c8e7a59060a69
PERCY GERONIMO DIONICIO <percy.geronimo.d@gmail.com>
2024-08-18 18:52:47 -0500

    a

--- services/revisions.ts
+++ services/revisions.ts
@@ -1,27 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_URL = 'https://your-api-url.com';
+// const API_URL = 'https://your-api-url.com';
 
-export const getRevisions = async () => {
-  const response = await axios.get(`${API_URL}/revisions`);
-  return response.data;
-};
+// export const getRevisions = async () => {
+//   const response = await axios.get(`${API_URL}/revisions`);
+//   return response.data;
+// };
 
-export const getRevisionById = async (id: number) => {
-  const response = await axios.get(`${API_URL}/revisions/${id}`);
-  return response.data;
-};
+// export const getRevisionById = async (id: number) => {
+//   const response = await axios.get(`${API_URL}/revisions/${id}`);
+//   return response.data;
+// };
 
-export const addRevision = async (revision: any) => {
-  const response = await axios.post(`${API_URL}/revisions`, revision);
-  return response.data;
-};
+// export const addRevision = async (revision: any) => {
+//   const response = await axios.post(`${API_URL}/revisions`, revision);
+//   return response.data;
+// };
 
-export const updateRevision = async (id: number, revision: any) => {
-  const response = await axios.put(`${API_URL}/revisions/${id}`, revision);
-  return response.data;
-};
+// export const updateRevision = async (id: number, revision: any) => {
+//   const response = await axios.put(`${API_URL}/revisions/${id}`, revision);
+//   return response.data;
+// };
 
-export const deleteRevision = async (id: number) => {
-  await axios.delete(`${API_URL}/revisions/${id}`);
-};
+// export const deleteRevision = async (id: number) => {
+//   await axios.delete(`${API_URL}/revisions/${id}`);
+// };
